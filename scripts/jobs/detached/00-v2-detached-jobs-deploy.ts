a445a4cb13a25c13d6b2e38b2af223a522e59fbd
Luciano Bertenasco <lbertenasco@gmail.com>
2021-10-20 14:34:39 -0300

    feat: deploy detached jobs on ftm

--- scripts/jobs/detached/00-v2-detached-jobs-deploy.ts
+++ scripts/jobs/detached/00-v2-detached-jobs-deploy.ts
@@ -30,15 +30,15 @@ function promptAndSubmit(): Promise<void | Error> {
             contracts.v2Keeper[networkName], // address _v2Keeper,
             60 * 5 // 5 minutes // uint256 _workCooldown
           );
-          const tendV2Keep3rJob = await TendV2DetachedJob.deploy(
+          const tendV2DetachedJob = await TendV2DetachedJob.deploy(
             contracts.baseFeeOracle[networkName],
             contracts.mechanicsRegistry[networkName],
             contracts.yOracle[networkName],
             contracts.v2Keeper[networkName],
             60 * 5 // 5 minutes // uint256 _workCooldown
           );
-          console.log('TendV2DetachedJob address:', tendV2Keep3rJob.address);
-          console.log(`PLEASE: change utils/contracts.ts tendV2Keep3rJob.${networkName} address to: ${tendV2Keep3rJob.address}`);
+          console.log('TendV2DetachedJob address:', tendV2DetachedJob.address);
+          console.log(`PLEASE: change utils/contracts.ts tendV2DetachedJob.${networkName} address to: ${tendV2DetachedJob.address}`);
           console.log();
 
           console.log(
@@ -50,7 +50,7 @@ function promptAndSubmit(): Promise<void | Error> {
             contracts.v2Keeper[networkName], // address _v2Keeper
             6 * 60 * 60 // uint256 _workCooldown // 6 hours
           );
-          const harvestV2Keep3rJob = await HarvestV2DetachedJob.deploy(
+          const harvestV2DetachedJob = await HarvestV2DetachedJob.deploy(
             contracts.baseFeeOracle[networkName], // address _baseFeeOracle,
             contracts.mechanicsRegistry[networkName], // address _mechanicsRegistry,
             contracts.stealthRelayer.mainnet, // address _stealthRelayer
@@ -58,8 +58,8 @@ function promptAndSubmit(): Promise<void | Error> {
             contracts.v2Keeper[networkName], // address _v2Keeper
             6 * 60 * 60 // uint256 _workCooldown // 6 hours
           );
-          console.log('HarvestV2DetachedJob address:', harvestV2Keep3rJob.address);
-          console.log(`PLEASE: change utils/contracts.ts harvestV2Keep3rJob.${networkName} address to: ${harvestV2Keep3rJob.address}`);
+          console.log('HarvestV2DetachedJob address:', harvestV2DetachedJob.address);
+          console.log(`PLEASE: change utils/contracts.ts harvestV2DetachedJob.${networkName} address to: ${harvestV2DetachedJob.address}`);
 
           resolve();
         } catch (err) {
